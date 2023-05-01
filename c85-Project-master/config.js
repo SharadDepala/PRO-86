dde2ec5131aed11c7f08302a1649281a440fb938
SharadDepala <98765045+SharadDepala@users.noreply.github.com>
2023-05-01 16:49:47 +0100

    Add files via upload

---
+++ c85-Project-master/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBAyRAOhdTL1NkdFv--qs4apgdRSKBEmMg",
+  authDomain: "p-85-d3d1a.firebaseapp.com",
+  projectId: "p-85-d3d1a",
+  storageBucket: "p-85-d3d1a.appspot.com",
+  messagingSenderId: "1052350248364",
+  appId: "1:1052350248364:web:a750b2222dfb559fb4f609"
+};
+
+  
